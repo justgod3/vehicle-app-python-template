9feb594dde1dc93d4981e40ee88ab5417df0d9b4
god33 <15755022426@163.com>
2023-09-05 04:28:26 +0000

    first finish

--- app/src/main.py
+++ app/src/main.py
@@ -24,7 +24,7 @@ from sdv.util.log import (  # type: ignore
     get_opentelemetry_log_format,
 )
 from sdv.vdb.reply import DataPointReply
-from sdv.vehicle_app import VehicleApp
+from sdv.vehicle_app import VehicleApp, subscribe_topic
 from vehicle import Vehicle, vehicle  # type: ignore
 
 # Configure the VehicleApp logger with the necessary log config and level.
@@ -34,7 +34,9 @@ logging.getLogger().setLevel("DEBUG")
 logger = logging.getLogger(__name__)
 
 
-DATABROKER_SUBSCRIPTION_TEMPERATURE_TOPIC = "sampleapp/currentTemperature"
+DATABROKER_SUBSCRIPTION_TEMPERATURE_TOPIC = "secondbox/currentTemperature"
+USER_ONLINE_REQUEST = "secondbox/userOnline/Request"
+USER_ONLINE_RESPONSE = "secondbox/userOnline/Response"
 
 
 class SecondBoxApp(VehicleApp):
@@ -66,24 +68,94 @@ class SecondBoxApp(VehicleApp):
         # the same callback.
         temperature = data.get(self.Vehicle.Cabin.HVAC.AmbientAirTemperature).value
 
-        # business processing
-        if temperature >= 28:
-            await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set('OPEN')
-        else:
-            await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set('CLOSE')
-        if temperature >= 30:
-            await self.Vehicle.Cabin.Sunroof.Switch.set('OPEN')
-        else:
-            await self.Vehicle.Cabin.Sunroof.Switch.set('CLOSE')
-        if temperature >= 32:
-            await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(35)
-        else:
-            await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(0)
+        logger.info("temperature value is, %s" % temperature)
+        response_data = {
+            "status": 1,
+            "messages": f"current temperature is {temperature}",
+        }
+        json_response_data = json.dumps(response_data)
+        await self.publish_event(DATABROKER_SUBSCRIPTION_TEMPERATURE_TOPIC,
+                                 json_response_data)
 
-        await self.publish_event(
-            DATABROKER_SUBSCRIPTION_TEMPERATURE_TOPIC,
-            json.dumps({"currentTemperature": temperature})
-        )
+    async def device_start(self, status_str) -> None:
+        # business processing
+        temperature = (await
+                       self.Vehicle.Cabin.HVAC.AmbientAirTemperature.get()
+                       ).value
+        logger.info("current temperature value is, %s" % temperature)
+        window_value = (await
+                        self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.get()).value
+        logger.info("window value is %s" % window_value)
+        roof_value = (await self.Vehicle.Cabin.Sunroof.Switch.get()).value
+        logger.info("roof value is %s" % roof_value)
+        fanspeed_value = (await
+                          self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.get()
+                          ).value
+        logger.info("fan speed value is %s" % fanspeed_value)
+        if status_str == "off":
+            if window_value != 'CLOSE' or window_value is not None:
+                await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set('CLOSE')
+            if roof_value != 'CLOSE' or roof_value is not None:
+                await self.Vehicle.Cabin.Sunroof.Switch.set('OPEN')
+            if fanspeed_value != 0:
+                await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(0)
+            # add AtmosphereLight close
+        else:
+            if temperature >= 28:
+                if window_value != 'OPEN':
+                    logger.info("window switch set open")
+                    await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set('OPEN')
+            else:
+                if window_value != 'CLOSE' or window_value is not None:
+                    logger.info("window switch set close")
+                    await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set('CLOSE')
+            if temperature >= 30:
+                if roof_value != 'OPEN':
+                    logger.info("sunroof switch set open")
+                    await self.Vehicle.Cabin.Sunroof.Switch.set('OPEN')
+            else:
+                if roof_value != 'CLOSE' or roof_value is not None:
+                    logger.info("sunroof switch set close")
+                    await self.Vehicle.Cabin.Sunroof.Switch.set('CLOSE')
+            if temperature >= 32:
+                if fanspeed_value != 35:
+                    logger.info("fanspeed set open")
+                    await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(35)
+            else:
+                if fanspeed_value != 0:
+                    logger.info("fanspeed set close")
+                    await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(0)
+
+    @subscribe_topic(USER_ONLINE_REQUEST)
+    async def user_online_request_received(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("user info is %s" % data)
+        status = data.get("status", None)
+        if status is None:
+            response_data = {
+                "status": 0,
+                "messages": "no expected args"
+            }
+            await self.publish_event(USER_ONLINE_RESPONSE, json.dumps(response_data))
+        if status == "on":
+            LOOP.create_task(self.device_start("on"))
+            response_data = {
+                "status": 1,
+                "messages": "device has start"
+            }
+        elif status == "off":
+            LOOP.create_task(self.device_start("off"))
+            response_data = {
+                "status": 1,
+                "messages": "device has stop"
+            }
+        else:
+            response_data = {
+                "status": 0,
+                "messages": "unexpected args"
+            }
+            
+        await self.publish_event(USER_ONLINE_RESPONSE, json.dumps(response_data))
 
 
 async def main():
