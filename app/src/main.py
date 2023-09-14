fa44f2eeba160c5ae14bc4fc2ae02d490e3dfd64
god33 <15755022426@163.com>
2023-09-14 10:27:50 +0000

    add code

--- app/src/main.py
+++ app/src/main.py
@@ -38,6 +38,22 @@ logger = logging.getLogger(__name__)
 DATABROKER_SUBSCRIPTION_TEMPERATURE_TOPIC = "secondbox/currentTemperature"
 USER_ONLINE_REQUEST = "secondbox/userOnline/Request"
 USER_ONLINE_RESPONSE = "secondbox/userOnline/Response"
+SET_MIRRORS_DIRECTION_REQUEST = "secondbox/setMirrorsDirecion/request"
+SET_MIRRORS_DIRECTION_RESPONSE = "secondbox/setMirrorsDirection/response"
+SET_FOLD_REQUEST = "secondbox/setFold/request"
+SET_FOLD_RESPONSE = "secondbox/setFold/response"
+SET_HVAC_REQUEST = "secondbox/setHvac/request"
+SET_HVAC_RESPONSE = "secondbox/setHvac/response"
+SET_DOOR_WINDOW_SWITCH_REQUEST = "secondbox/setDoorWindowSwitch/request"
+SET_DOOR_WINDOW_SWITCH_RESPONSE = "secondbox/ \
+    setDoorWindowSwitch/response"
+SET_SUNROOF_REQUEST = "secondbox/setSunroof/request"
+SET_SUNFOOF_RESPONSE = "secondbox/setSunroof/response"
+
+SET_SEATFAN_REQUEST = "secondbox/setSeatfan/request"
+SET_SEATFAN_RESPONSE = "secondbox/setSeatfan/response"
+SET_ISDOMEON_REQUEST = "secondbox/setDomeonLight/request"
+SET_ISDOMEON_RESPONSE = "secondbox/setDomeonLight/response"
 
 
 class SecondBoxApp(VehicleApp):
@@ -137,7 +153,6 @@ class SecondBoxApp(VehicleApp):
                 self.on_temperature_change))
             logger.info("subscribe data point")
 
-        
     @subscribe_topic(USER_ONLINE_REQUEST)
     async def user_online_request_received(self, data_str: str) -> None:
         data = json.loads(data_str)
@@ -166,8 +181,243 @@ class SecondBoxApp(VehicleApp):
                 "status": 0,
                 "messages": "unexpected args"
             }
-            
+
         await self.publish_event(USER_ONLINE_RESPONSE, json.dumps(response_data))
+
+    @subscribe_topic(SET_MIRRORS_DIRECTION_REQUEST)
+    async def set_mirrors_direction_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        direction_str = data.get("direction", None)
+        direction = direction_str.lower()
+        angle = data.get("angle", 0)
+        if direction not in ["up", "down", "left", "right"]:
+            response_data = {
+                "status": 0,
+                "messages": "unknow direction"
+            }
+        else:
+            if angle == 0:
+                response_data = {
+                    "status": 1,
+                    "messages": "angle is 0, do nothing"
+                }
+            else:
+                if direction == "left":
+                    pan_value = (await self.Vehicle.Body.Mirrors.Left.Pan.get()).value
+                    if pan_value == 100:
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors is already on the far left"
+                        }
+                    else:
+                        total_value = pan_value + angle
+                        if total_value >= 100:
+                            await self.Vehicle.Body.Mirrors.Left.Pan.set(100)
+                        else:
+                            await self.Vehicle.Body.Mirrors.Left.Pan.set(total_value)
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors has been turn left"
+                        }
+                elif direction == "right":
+                    pan_value = (await self.Vehicle.Body.Mirrors.Left.Pan.get()).value
+                    if pan_value == -100:
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors is already on the far left"
+                        }
+                    else:
+                        total_value = pan_value - angle
+                        if total_value <= -100:
+                            await self.Vehicle.Body.Mirrors.Left.Pan.set(-100)
+                        else:
+                            await self.Vehicle.Body.Mirrors.Left.Pan.set(total_value)
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors has been turn right"
+                        }
+                elif direction == "up":
+                    tilt_value = (await self.Vehicle.Body.Mirrors.Left.Tilt.get()).value
+                    if tilt_value == 100:
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors is already on the far up"
+                        }
+                    else:
+                        total_value = tilt_value + angle
+                        if total_value >= 100:
+                            await self.Vehicle.Body.Mirrors.Left.Tilt.set(100)
+                        else:
+                            await self.Vehicle.Body.Mirrors.Left.Tilt.set(total_value)
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors has been turn up"
+                        }
+                else:
+                    tilt_value = (await self.Vehicle.Body.Mirrors.Left.Tilt.get()).value
+                    if tilt_value == -100:
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors is already on the far down"
+                        }
+                    else:
+                        total_value = tilt_value - angle
+                        if total_value <= -100:
+                            await self.Vehicle.Body.Mirrors.Left.Tilt.set(-100)
+                        else:
+                            await self.Vehicle.Body.Mirrors.Left.Tilt.set(total_value)
+                        response_data = {
+                            "status": 1,
+                            "messages": "mirrors has been turn down"
+                        }
+        logger.info("mirrors turn %s,angle is %s" % (direction, angle))
+        await self.publish_event(
+            SET_MIRRORS_DIRECTION_RESPONSE,
+            json.dumps(response_data)
+        )
+
+    @subscribe_topic(SET_FOLD_REQUEST)
+    async def set_mirrors_fold_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data: %s" % data)
+        fold = data["fold"]
+        fold_value = (await self.Vehicle.Body.Mirrors.Left.Fold.get()).value
+        if fold in [0, 1]:
+            fold_status = "expand" if fold == 1 else "fold"
+            if fold == fold_value:
+                response_data = {
+                    "status": 1,
+                    "messages": f"current mirrors status is already {fold_status}"
+                }
+                logger.info("fold status current status is already %s" % fold_status)
+            else:
+                await self.Vehicle.Body.Mirrors.Left.Fold.set(fold)
+                response_data = {
+                    "status": 1,
+                    "messages": f"current mirror change {fold_status}"
+                }
+                logger.info("fold status has change %s" % fold_status)
+        else:
+            response_data = {
+                "status": 0,
+                "messages": "unpected values"
+            }
+            logger.info("unpected value,do nothing")
+
+        await self.publish_event(
+            SET_FOLD_RESPONSE,
+            json.dumps(response_data)
+        )
+
+    @subscribe_topic(SET_HVAC_REQUEST)
+    async def set_mirrors_hvac_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data is %s" % data)
+        fanspeed = data["fanspeed"]
+        fanspeed_val = (await
+                        self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.get()).value
+        if fanspeed < 0 or fanspeed > 100:
+            response_data = {
+                "status": 0,
+                "messages": "out of range value"
+            }
+            logger.info("out of range range")
+        else:
+            if fanspeed == fanspeed_val:
+                response_data = {
+                    "status": 1,
+                    "messages": f"""current fanspeed is already {fanspeed}"""
+                }
+                logger.info("current fanspeed is already %s" % fanspeed)
+            else:
+                await self.Vehicle.Cabin.HVAC.Station.Row1.Left.FanSpeed.set(fanspeed)
+                response_data = {
+                    "status": 1,
+                    "messages": f"""fanspeed is change {fanspeed}"""
+                }
+                logger.info("fanspeed is change %s" % fanspeed)
+        await self.publish_event(
+            SET_HVAC_RESPONSE,
+            json.dumps(response_data)
+        )
+
+    @subscribe_topic(SET_DOOR_WINDOW_SWITCH_REQUEST)
+    async def set_door_window_switch_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data is %s" % data)
+        door_windo_window = data["window_value"]
+        door_window_value = (await
+                             self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.get()
+                             ).value
+        if door_window_value == door_windo_window:
+            response_data = {
+                "status": 1,
+                "messages": f"""door window current status is {door_windo_window},
+                do nothing"""
+            }
+        else:
+            await self.Vehicle.Cabin.Door.Row1.Left.Window.Switch.set(door_windo_window)
+            response_data = {
+                "status": 1,
+                "messages": f"""door window has change {door_windo_window}"""
+            }
+
+        await self.publish_event(
+            SET_HVAC_RESPONSE,
+            json.dumps(response_data)
+        )
+    
+    @subscribe_topic(SET_SUNROOF_REQUEST)
+    async def set_mirrors_sunroof_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data is %s" % data)
+        sunroof = data["sunroof"]
+        sunroof_val = (await self.Vehicle.Cabin.Sunroof.Switch.get()).value
+        if sunroof == sunroof_val:
+            response_data = {
+                "status": 1,
+                "messages": """sunroof current status is already {sunroof}"""
+            }
+        else:
+            await self.Vehicle.Cabin.Sunroof.Switch.set(sunroof)
+            response_data = {
+                "status": 1,
+                "messages": f"""sunroof status has change {sunroof}"""
+            }
+
+        await self.publish_event(
+            SET_SUNFOOF_RESPONSE,
+            json.dumps(response_data)
+        )
+
+    @subscribe_topic(SET_SEATFAN_REQUEST)
+    async def set_seatfan_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data is %s" % data)
+        pass
+
+    @subscribe_topic(SET_ISDOMEON_REQUEST)
+    async def set_domeon_change(self, data_str: str) -> None:
+        data = json.loads(data_str)
+        logger.info("data is %s" % data)
+        light_status = data["light_status"]
+        domeon_value = (await self.Vehicle.Cabin.Lights.IsDomeOn.get()).value
+        light_status_val = "on" if light_status is True else "off"
+        if light_status == domeon_value:
+            response_data = {
+                "status": 1,
+                "messages": f"""current light status is {light_status_val}"""
+            }
+        else:
+            await self.Vehicle.Cabin.Lights.IsDomeOn.set(light_status)
+            response_data = {
+                "status": 1,
+                "messages": f"""light status has chang {light_status_val}"""
+            }
+        await self.publish_event(
+            SET_ISDOMEON_RESPONSE,
+            json.dumps(response_data)
+        )
 
 
 async def main():
